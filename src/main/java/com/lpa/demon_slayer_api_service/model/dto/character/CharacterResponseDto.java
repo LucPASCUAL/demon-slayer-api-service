9ec117b3eef3d04a790423cf8e5cb15f49e82b0c
LucPSC <luc.pascual1997@gmail.com>
2025-11-25 12:41:09 +0100

    fix(javadoc): '*' alignment in JavaDoc syntax

--- src/main/java/com/lpa/demon_slayer_api_service/model/dto/character/CharacterResponseDto.java
+++ src/main/java/com/lpa/demon_slayer_api_service/model/dto/character/CharacterResponseDto.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
   *
   * @param content the JSON Array key in the body of HTTP response that contains a {@link CharacterDto}
- */
+  */
 @JsonIgnoreProperties(ignoreUnknown = true) //Jackson will ignore any additional fields present in the JSON
 public record CharacterResponseDto(
         List<CharacterDto> content
--- src/main/java/com/lpa/demon_slayer_api_service/model/dto/pagination/PageResponseCombatStyleDto.java
+++ src/main/java/com/lpa/demon_slayer_api_service/model/dto/pagination/PageResponseCombatStyleDto.java
@@ -7,10 +7,10 @@ import com.lpa.demon_slayer_api_service.model.dto.CombatStyleDto;
 import java.util.List;
 
 /**
- *
- * @param pagination   The JSON Object key in the body of the HTTP response that contains a {@link PaginationDto}
- * @param content      The JSON Array key in the body of the HTTP response that contains a List of {@link CombatStyleDto}
- */
+  *
+  * @param pagination   The JSON Object key in the body of the HTTP response that contains a {@link PaginationDto}
+  * @param content      The JSON Array key in the body of the HTTP response that contains a List of {@link CombatStyleDto}
+  */
 @JsonIgnoreProperties(ignoreUnknown = true) //Jackson will ignore any additional fields present in the JSON
 public record PageResponseCombatStyleDto(
         PaginationDto pagination,
